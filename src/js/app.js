5b8a1b518a6554576e9e0ab7acc7ac27a9fa104d
Johan Andres Cardenas Hernandez <tecnogas2010@gmail.com>
2026-03-15 17:18:53 +0000

    Final conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -1,81 +1,81 @@
-import "../style/index.css";
+window.variables = {
+  includeCover: false,
+  background: null,
+  avatarURL: "https://randomuser.me/api/portraits/women/44.jpg",
+  socialMediaPosition: "right",
+  twitter: null,
+  github: null,
+  linkedin: null,
+  instagram: null,
+  name: "Ramon",
+  lastName: null,
+  role: "Web Developer",
+  country: "USA",
+  city: "Miami"
+};
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "position-right", // social media bar position (position-left or position-right)
-        //for social media links, only update usernames
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  const name = variables.name || "Your name";
+  const lastName = variables.lastName || "Your lastname";
+  const role = variables.role || "Web Developer";
+  const city = variables.city || "Miami";
+  const country = variables.country || "USA";
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  const includeCover = variables.includeCover === true;
+
+  const cover = includeCover
+    ? `<div class="cover">
+        <img src="${variables.background ||
+          "https://images.unsplash.com/photo-1492724441997-5dc865305da7"}" />
+      </div>`
+    : `<div class="cover hidden"></div>`;
+
+  const socialPosition =
+    variables.socialMediaPosition === "left" ? "left" : "right";
+
+  const twitter = variables.twitter
+    ? `<a href="https://twitter.com/${variables.twitter}" target="_blank"><i class="fab fa-twitter"></i></a>`
+    : `<a class="disabled"><i class="fab fa-twitter"></i></a>`;
+
+  const github = variables.github
+    ? `<a href="https://github.com/${variables.github}" target="_blank"><i class="fab fa-github"></i></a>`
+    : `<a class="disabled"><i class="fab fa-github"></i></a>`;
+
+  const linkedin = variables.linkedin
+    ? `<a href="https://linkedin.com/in/${variables.linkedin}" target="_blank"><i class="fab fa-linkedin-in"></i></a>`
+    : `<a class="disabled"><i class="fab fa-linkedin-in"></i></a>`;
+
+  const instagram = variables.instagram
+    ? `<a href="https://instagram.com/${variables.instagram}" target="_blank"><i class="fab fa-instagram"></i></a>`
+    : `<a class="disabled"><i class="fab fa-instagram"></i></a>`;
+
+  document.querySelector("#widget_content").innerHTML = `
+    <div class="widget ${includeCover ? "has-cover" : ""}">
+      ${cover}
+
+      <div class="social-bar ${socialPosition}">
+        ${twitter}
+        ${github}
+        ${linkedin}
+        ${instagram}
+      </div>
+
+      <img src="${variables.avatarURL}" class="photo"/>
+
+      <h1>${name} ${lastName}</h1>
+      <h2>${role}</h2>
+      <h3>${city}, ${country}</h3>
+    </div>
+  `;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
-  window.variables = {
-    // if includeCover is true the algorithm should show the cover image
-    includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (position-left or position-right)
-    socialMediaPosition: "position-right",
-    // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
-  };
-  render(window.variables); // render the card for the first time
+  render(window.variables);
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      const attribute = e.target.getAttribute("for");
+
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -85,7 +85,8 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+
+      render(Object.assign(window.variables, values));
     });
   });
 };
